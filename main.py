8a8710f27118e150a903f1c36f06c99e878ce9aa
iiivaaaann <i.garcial.2020@alumnos.urjc.es>
2023-03-07 18:35:29 +0100

    Tablas creadas.

--- main.py
+++ main.py
@@ -10,10 +10,19 @@ cur=conexion.cursor()
 #conexion.commit()
 #cur.execute("CREATE TABLE IF NOT EXISTS DEVICES (id text PRIMARY KEY, ip text, localizacion text, responsable text)")
 #cur.execute("CREATE TABLE PUERTOS (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre text)")
+##cur.execute("INSERT INTO PUERTOS (nombre) VALUES ('puerto de prueba')")
+#cur.execute("DROP TABLE PUERTOS")
+#  FOREIGN KEY(trackartist) REFERENCES artist(artistid)
+cur.execute("CREATE TABLE if not exists RESPONSABLE (nombre text PRIMARY KEY, telefono int, rol text)")
+cur.execute("CREATE TABLE if not exists DEVICES (id text primary key , ip text, responsable_nombre text, FOREIGN KEY (responsable_nombre) references responsable(nombre))")
+cur.execute("CREATE TABLE if not exists ANALISIS (ID INTEGER PRIMARY KEY autoincrement, devices_id text, servicios int, servicios_ins int, detect_vulns int, FOREIGN KEY (devices_id) references DEVICES(id))")
+cur.execute("CREATE TABLE if not exists PUERTOS (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre text,analisis_id int, FOREIGN KEY (analisis_id) REFERENCES ANALISIS(id))")
+
 with open("devices.json") as f:
     devices=json.load(f) # array en devices
     for i in devices:
        for j in i["analisis"]["puertos_abiertos"]:
            if j not in('N','o','n','e'):
-            print(j)
-            cur.execute("INSERT INTO PUERTOS (nombre) VALUES(?)", j)
+            #cur.execute("INSERT INTO PUERTOS (nombre) VALUES (?)",(j,) )
+            conexion.commit()
+

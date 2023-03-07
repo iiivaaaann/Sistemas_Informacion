f33a5650d56e6ff94744f659f268b1a76020edba
iiivaaaann <i.garcial.2020@alumnos.urjc.es>
2023-03-07 17:16:36 +0100

    ..

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-import sqlite3
 import pandas as pd
-
-#df=pd.read_csv('alerts.csv',delimiter=',')
-#print(df)
+import sqlite3
 conexion= sqlite3.connect('pr1_SI.db')
 cur=conexion.cursor()
 #cur.execute("CREATE TABLE ALERTS (time date, sid int, msg text, clasification text, priority int, protocolo text, origen text, destino text, puerto int)")
 #cur.execute("INSERT INTO ALERTS VALUES('10/10/2022', 1, 'ASD', 'IOP', 1, 'TCPPCPCP','123.123.123','234.23.24',787)")
 #cur.execute("DELETE FROM ALERTS WHERE SID='1'")
-conexion.commit()
-
-#
-cur.execute('SELECT puerto FROM ALERTS ORDER BY puerto ASC ')
-conexion.commit()
+#conexion.commit()

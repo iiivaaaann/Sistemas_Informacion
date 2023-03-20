a826700ee8686e3ad888e1ed593e7fe8a980ad52
iiivaaaann <i.garcial.2020@alumnos.urjc.es>
2023-03-20 18:41:48 +0100

    Insercion de puertos, UNIQUE CONSTRAINT ADD.

--- main.py
+++ main.py
@@ -3,22 +3,11 @@ import sqlite3
 import json
 conexion= sqlite3.connect('pr1_SI.db')
 cur=conexion.cursor()
-#cur.execute("DROP TABLE ALERTS")
-#cur.execute("CREATE TABLE ALERTS (id INTEGER PRIMARY KEY AUTOINCREMENT, time date, sid int , msg text, clasification text, priority int, protocolo text, origen text, destino text, puerto int)")
-#cur.execute("INSERT INTO ALERTS VALUES('10/10/2022', 1, 'ASD', 'IOP', 1, 'TCPPCPCP','123.123.123','234.23.24',787)")
-#cur.execute("DELETE FROM ALERTS WHERE SID='1'")
-#conexion.commit()
-#cur.execute("CREATE TABLE IF NOT EXISTS DEVICES (id text PRIMARY KEY, ip text, localizacion text, responsable text)")
-#cur.execute("CREATE TABLE PUERTOS (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre text)")
-##cur.execute("INSERT INTO PUERTOS (nombre) VALUES ('puerto de prueba')")
-#cur.execute("DROP TABLE PUERTOS")
-#  FOREIGN KEY(trackartist) REFERENCES artist(artistid)
-cur.execute("DROP TABLE RESPONSABLE")
+
 cur.execute("CREATE TABLE if not exists RESPONSABLE (nombre text PRIMARY KEY, telefono int, rol text)")
 cur.execute("CREATE TABLE if not exists DEVICES (id text primary key , ip text, responsable_nombre text, FOREIGN KEY (responsable_nombre) references responsable(nombre))")
-cur.execute("CREATE TABLE if not exists ANALISIS (ID INTEGER PRIMARY KEY autoincrement, devices_id text, servicios int, servicios_ins int, detect_vulns int, FOREIGN KEY (devices_id) references DEVICES(id))")
-cur.execute("DROP TABLE PUERTOS")
-cur.execute("CREATE TABLE if not exists PUERTOS (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre text,analisis_id int NOT NULL, FOREIGN KEY (analisis_id) REFERENCES ANALISIS(id))")
+cur.execute("CREATE TABLE if not exists ANALISIS (ID INTEGER PRIMARY KEY autoincrement, devices_id text UNIQUE, servicios int, servicios_ins int, detect_vulns int, FOREIGN KEY (devices_id) references DEVICES(id))")
+cur.execute("CREATE TABLE if not exists PUERTOS (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre text,analisis_id int NOT NULL, FOREIGN KEY (analisis_id) REFERENCES ANALISIS(id) CONSTRAINT NOT_REPEATED_PORTS_ANALISIS UNIQUE (nombre, analisis_id))")
 
 with open("devices.json") as f:
     devices=json.load(f)
@@ -43,11 +32,27 @@ with open("devices.json") as f:
     b = cur.execute("SELECT COUNT(*) FROM DEVICES").fetchall()
     print("....devices insertados con éxito!!")
     print(str(b[0][0]) + " devices en la BBDD")
-
-
-
+    print("Procediendo a insertar ANÁLISIS.....")
+    for i in devices:
+        cur.execute("INSERT OR IGNORE INTO ANALISIS (DEVICES_ID, SERVICIOS, SERVICIOS_INS,DETECT_VULNS) VALUES (?,?,?,?)", (i["id"],i["analisis"]["servicios"],i["analisis"]["servicios_inseguros"], i["analisis"]["vulnerabilidades_detectadas"]))
+    print("..... análisis insertados")
+    b = cur.execute("SELECT COUNT(*) FROM DEVICES").fetchall()
+    print(str(b[0][0])+" analisis en la BBDD")
+#Para insertar en la tabla puertos debemos ver qué puertos abiertos hay en X dispositivo y buscar su analisis_id en la BD
+    for i in devices:
+        #print("Device id: " + i["id"])
+        #print("Analisis id: " + str(cur.execute("SELECT ID FROM ANALISIS WHERE DEVICES_ID=?", (i["id"],)).fetchall()[0][0]))
+        #print("Puertos abiertos: " + str(i["analisis"]["puertos_abiertos"]))
+        analisis_id=cur.execute("SELECT ID FROM ANALISIS WHERE DEVICES_ID=?", (i["id"],)).fetchall()[0][0]
+        for j in i["analisis"]["puertos_abiertos"]:
+            if i["analisis"]["puertos_abiertos"]!="None":
+                name=j
+                #print("nombre: " + name + " id analisis: " + str(analisis_id))
+                cur.execute("INSERT OR IGNORE INTO PUERTOS (NOMBRE, ANALISIS_ID) VALUES (?,?)",(name,analisis_id))
+    print(".... puertos insertados correctamente!")
 conexion.commit()
 
+exit(0)
 #Ejer 2
 
 #1. Número de dispositivos

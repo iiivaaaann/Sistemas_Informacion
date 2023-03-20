6c3b4f1c5a01540daa6089d3ff4b5b4e9fc455fb
iiivaaaann <i.garcial.2020@alumnos.urjc.es>
2023-03-20 17:31:07 +0100

    Insercion de devices y de responsables

--- main.py
+++ main.py
@@ -13,16 +13,38 @@ cur=conexion.cursor()
 ##cur.execute("INSERT INTO PUERTOS (nombre) VALUES ('puerto de prueba')")
 #cur.execute("DROP TABLE PUERTOS")
 #  FOREIGN KEY(trackartist) REFERENCES artist(artistid)
+cur.execute("DROP TABLE RESPONSABLE")
 cur.execute("CREATE TABLE if not exists RESPONSABLE (nombre text PRIMARY KEY, telefono int, rol text)")
 cur.execute("CREATE TABLE if not exists DEVICES (id text primary key , ip text, responsable_nombre text, FOREIGN KEY (responsable_nombre) references responsable(nombre))")
 cur.execute("CREATE TABLE if not exists ANALISIS (ID INTEGER PRIMARY KEY autoincrement, devices_id text, servicios int, servicios_ins int, detect_vulns int, FOREIGN KEY (devices_id) references DEVICES(id))")
-cur.execute("CREATE TABLE if not exists PUERTOS (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre text,analisis_id int, FOREIGN KEY (analisis_id) REFERENCES ANALISIS(id))")
+cur.execute("DROP TABLE PUERTOS")
+cur.execute("CREATE TABLE if not exists PUERTOS (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre text,analisis_id int NOT NULL, FOREIGN KEY (analisis_id) REFERENCES ANALISIS(id))")
 
 with open("devices.json") as f:
-    devices=json.load(f) # array en devices
+    devices=json.load(f)
+    print("Insertando responsables......")
     for i in devices:
-       for j in i["analisis"]["puertos_abiertos"]:
-           if j not in('N','o','n','e'):
-            #cur.execute("INSERT INTO PUERTOS (nombre) VALUES (?)",(j,) )
-            conexion.commit()
+       if i["responsable"]["telefono"]=="None":
+           t=None
+       else: ## Este bucle es pq si no inserta la frase NONE en lugar de meter null y para analizar es mas sencillo con Null (ademas none no es un dato valido).
+           t = i["responsable"]["telefono"]
+       if i["responsable"]["rol"]=="None":
+           r=None
+       else:
+           r=i["responsable"]["rol"]
+
+       cur.execute("INSERT OR IGNORE INTO RESPONSABLE (nombre, telefono, rol) VALUES (?,?,?)",(i["responsable"]["nombre"], t, r))
+    b=cur.execute("SELECT COUNT(*) FROM RESPONSABLE").fetchall()
+    print("....responsables insertados con éxito!!")
+    print(str(b[0][0])+" responsables en la BBDD")
+    print("Procedemos a insertar DEVICES!")
+    for i in devices:
+       cur.execute("INSERT OR IGNORE INTO DEVICES (id, IP, responsable_nombre) VALUES (?,?,?)",(i["id"], i["ip"],i["responsable"]["nombre"]))
+    b = cur.execute("SELECT COUNT(*) FROM DEVICES").fetchall()
+    print("....devices insertados con éxito!!")
+    print(str(b[0][0]) + " devices en la BBDD")
+
+
+
+conexion.commit()
 

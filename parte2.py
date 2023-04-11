37292a32789735c06441627062daeec953ba13d3
elady56 <elady02@gmail.com>
2023-04-11 17:32:58 +0200

    Estructura

--- parte2.py
+++ parte2.py
@@ -5,6 +5,18 @@ app = Flask(__name__)
 def hello_world():
     return '<h1>Hello, World! <ul><li>Hola juan</li></ul></h1>'
 
+def ejer2():
+    print("Ejer 2")
+
+def ejer3():
+    print("Ejer 3")
+
+def ejer4():
+    print("Ejer 4")
+
+def ejer5():
+    print("Ejer 5")
+
 
 if __name__ == '__main__':
     app.run(debug=True)

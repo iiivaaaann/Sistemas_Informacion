eb062b66addf6b3e6777e56efd2402fc706239d2
iiivaaaann <i.garcial.2020@alumnos.urjc.es>
2023-04-19 18:07:02 +0200

    Ejercicio 4 parte3

--- parte2.py
+++ parte2.py
@@ -1,11 +1,14 @@
+import pdfkit
 import requests
 import pandas as pd
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file, make_response, Response
 import funciones
 import sqlite3
-from IPython.display import HTML
-
-
+from xhtml2pdf import pisa
+from api2pdf import Api2Pdf
+API2PDF_API_KEY='5a12d7c8-9255-47a1-aaf9-a73ead097999'
+USERAGENT = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 conexion= sqlite3.connect('pr1_SI.db', check_same_thread=False)
 cur=conexion.cursor()
 
@@ -20,8 +23,11 @@ def ejercicio1():
     if request.method == "POST":
         numIP = request.form['numIP']
         numDisp = request.form['numDisp']
-        f1=funciones.obtenerTopIps(int(numIP), conexion)
-        f2=funciones.obtenerTopDispositivos(int(numDisp), conexion)
+        f1=f2=None
+        if numIP:
+            f1=funciones.obtenerTopIps(int(numIP), conexion)
+        if numDisp:
+            f2=funciones.obtenerTopDispositivos(int(numDisp), conexion)
         return render_template("ejercicio1.html", numIP=numIP, numDisp=numDisp, f1=f1, f2=f2)
     elif request.method == "GET":
         return render_template("ejercicio1.html")
@@ -32,18 +38,26 @@ def ejercicio2():
     return render_template("ejercicio2.html")
 
 @app.route('/ejercicio3')
-def ejercicio3():
+def ejercicio3(pdf=False):
     print("Ejercicio 3")
     response = requests.get("https://cve.circl.lu/api/last/10").text
     df = pd.read_json(response)
     df = df.iloc[:10]
     df = df.iloc[:, [0, 1, 3, 6, 7, 9, 10]]
-    return render_template("ejercicio3.html", tables=[df.to_html()])
+    if not pdf:
+        return render_template("ejercicio3.html", tables=[df.to_html()])
+    elif pdf:
+        return Response(df.to_html())
+
 
 @app.route('/ejercicio4')
-def ejercicio4():
-    print("Ejercicio 4")
-    return render_template("ejercicio4.html")
+def ejercicio4(): ## Para usar esto es necesario instalar wkhtmltopdf con sudo apt-get o brew. para windows buscar xd ; de momento solo genera el pdf del ejercicio 3
+    html = ejercicio3(True).data.decode('utf-8')
+    pdf = pdfkit.from_string(html, False, options={'quiet': ''})
+    response = make_response(pdf)
+    response.headers["Content-Type"] = "application/pdf"
+    response.headers["Content-Disposition"] = "inline; filename=output.pdf"
+    return response
 
 @app.route('/ejercicio5')
 def ejercicio5():
